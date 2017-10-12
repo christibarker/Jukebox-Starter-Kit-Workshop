2c85c2819abd38334ff196c2e1d2230497716f95
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-12 16:30:24 -0600

    javascript

--- index.js
+++ index.js
@@ -3,39 +3,57 @@
 // window.open(URL, WindowName, Features);
 // }
 
-// function playSong(){
-// 	var audio = getElementsById('audio').addEventListener('onclick');
-// 	var buttonPlay = getElementsById('play').addEventListener('click', audio, false);
-// 	var buttonStop = getElementsById('stop').addEventListener('click', audio);
-// 	if (audio.paused) {
-// 		audio.play();
-// 		button.textContent = " || "
-// 	}
-// 	else {
-// 		audio.pause();
-// 		button.textContent = ">";
-// 	}
-// 	else {
-// 		audio.stop();
-
-// 	}
-// }
 
 class JukeBox {
-	contstructor() {
-		this.song = [];
+	constructor() {
+		this.audio = document.getElementsByTagName('audio')[0];
+	}
+
+	playSong() {
+		console.log(this)
+		this.audio.play();
+	}
+
+	stopSong() {
+		this.audio.pause();
+	}
+
+	loadSong(loadSong) {
+		this.audio.setAttribute('src', loadSong);
 	}
 }
 
-addSongs(song){
-	this.song.push(song);
-}
-
-
-
-
-
-
 
 var playlist = new JukeBox();
+
+// addEventListeners added here
+
+document.getElementById('play').addEventListener('click', function(){
+	playlist.playSong();
+		// if (play.paused) {
+		// 	playSong.play();
+		// 	// button.textContent = ' || ';
+		// } else {
+		// 	playSong.pause();
+		// 	// button.textContent = '>';
+		// }
+});
+
+document.getElementById('stop').addEventListener('click', function(){
+	playlist.stopSong();
+})
+
+
+
+document.getElementById('list').addEventListener('click', function(){
+playlist.loadSong('audio/Happy.mp3');
+playlist.loadSong('audio/Best_Day_of_My_Life.mp3');
+playlist.loadSong('audio/Footloose.mp3');	
+})
+
+
+playlist.loadSong('audio/Happy.mp3');
+playlist.loadSong('audio/Best_Day_of_My_Life.mp3');
+playlist.loadSong('audio/Footloose.mp3');
+
 
