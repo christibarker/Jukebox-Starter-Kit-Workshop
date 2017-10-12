d545c5169d56071388d369a68910552600d9b4b0
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-12 11:21:13 -0600

    htmlformat

---
+++ index.js
@@ -0,0 +1,41 @@
+// function openjukebox (URL, WindowName, Features) 
+// { 
+// window.open(URL, WindowName, Features);
+// }
+
+// function playSong(){
+// 	var audio = getElementsById('audio').addEventListener('onclick');
+// 	var buttonPlay = getElementsById('play').addEventListener('click', audio, false);
+// 	var buttonStop = getElementsById('stop').addEventListener('click', audio);
+// 	if (audio.paused) {
+// 		audio.play();
+// 		button.textContent = " || "
+// 	}
+// 	else {
+// 		audio.pause();
+// 		button.textContent = ">";
+// 	}
+// 	else {
+// 		audio.stop();
+
+// 	}
+// }
+
+class JukeBox {
+	contstructor() {
+		this.song = [];
+	}
+}
+
+addSongs(song){
+	this.song.push(song);
+}
+
+
+
+
+
+
+
+var playlist = new JukeBox();
+
